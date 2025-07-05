3e9b0a68ab1ee0067b6e454b8bb7c939fefca995
Smirti Neupane <smirtineupane197@gmail.com>
2025-07-05 23:48:08 +0545

    Initial commit: added day1, package.json, and notes

---
+++ Day1/app.js
@@ -0,0 +1,23 @@
+// function add(a,b){
+//     return a+b;
+// }
+// function sub (a,b){
+//     return a-b;
+// }
+// // add function lai export agreyko 
+// module.exports = {add,sub}
+
+// shortland using exports 
+
+// exports.add = (a,b)=>{
+//     return a+b;
+// }
+// exports.sub = (a,b) =>{
+//     return a - b;
+// }
+// const user = {
+//     name:"smriti",
+//     age:21,
+//     aim:"software developer"
+// }
+// module.exports = user;
---
+++ Day1/index.js
@@ -0,0 +1,45 @@
+// console.log("hello from node.js");
+
+// const { use } = require("react");
+
+// Node.js Built-in Global Objects
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// global.name = "Smriti";
+// console.log(name); 
+// console.log(module);
+
+// global.name = "smriti neupane"
+
+// function display(){
+//     global.user = "sita"
+//     console.log("hello", name);
+//     console.log(name);
+    
+// }
+
+// display()
+//     console.log("hello",user);
+
+// console.log(process.platform);
+// console.log(process.env);
+
+
+// require : package lai use garna we sue require
+
+// latest module : ecma script 
+
+// import from app.js 
+
+// note:run the file that contains require 
+
+// const {add,sub,user} = require("./app")
+
+// console.log(add(2,3));
+// console.log(sub(3,2));
+// console.log("user info:",user);
+
+
+
