c2060880466aa7000057d33824ab23442eea5b27
Smirti Neupane <smirtineupane197@gmail.com>
2025-07-10 21:19:03 +0545

    Updated index.js with OS, Path, fs, and EventEmitter practice

--- Day2/index.js
+++ Day2/index.js
@@ -1,8 +1,48 @@
-// Builtin-module 
+// Builtin-module
 
-// Node.js has a built-in module called fs to work with files 
+// Node.js has a built-in module called fs to work with files
 
-// 1 using fs module 
+
+// The OS Module
+// const { log } = require('console');
+// const os = require('os')
+
+// const systemuptime = os.uptime()
+
+// const userInfo = os.userInfo()
+
+// const otherInfo = {
+//     name: os.type(),
+//     release: os.release(),
+//     totalMem: os.totalmem(),
+//     freeMem: os.freemem(),
+// }
+// console.log(systemuptime);
+// console.log(userInfo);
+// console.log(otherInfo);
+
+
+// The PATH Module
+// const path = require('path')
+
+// const mypath = 'backend-learning\Day2\index.js'
+
+// const pathInfo = {
+//     fileName: path.basename(mypath),
+//     foldername: path.dirname(mypath),
+//     fileExtension: path.extname(mypath),
+//     absoluteorNot: path.isAbsolute(mypath),
+//     detailInfo: path.parse(mypath)
+
+// }
+// console.log(pathInfo);
+// console.log(path.join('grandParentFolder', 'parentFolder', 'child.txt'))
+// console.log(path.resolve('grandParentFolder', 'parentFolder', 'child.txt'))
+
+
+
+
+// The FS Module
 // const fs = require('fs')
 // fs.writeFileSync('day1.txt','Hello User')
 // console.log("File created successfully");
@@ -22,9 +62,28 @@
 // })
 
 
-// read,write,append,delete 
+// / {flag: 'a'} indicates the writeFile() method to append 
+// the data at the end of the file instead of erasing the 
+// previous data present in it.
 
-// const { error } = require('console');
+// const fs = require('fs')
+// fs.writeFile('user.txt', "Hi,this is newFile.txt",{flag: 'a'}, (error) => {
+//     if (error) {
+//         console.log(error);
+
+
+//     } else {
+//         console.log('Writen to file successfully');
+
+
+//     }
+// })
+
+
+
+
+// read,write,append,delete
+
 // const fs = require('fs')
 // fs.writeFileSync('user.txt',"Hello user")
 // console.log("File created successfully");
@@ -51,7 +110,7 @@
 
 // })
 
-// //append file 
+// //append file
 // fs.appendFile('user.txt','backend learning',(error)=>{
 //     if(error){
 //         console.log("error occurred",error);
@@ -75,7 +134,30 @@
 //     }
 // })
 
-// url 
+// How to read the contents of a directory using fs.readdir()
+
+// The readdir() function accepts 2 parameters:
+
+// The path of the folder whose contents are to be read.
+// Callback function.
+
+// const fs = require('fs')
+// fs.readdir('D:/Nodejs-workshop/backend-learning/Day2',(error,files)=>{
+//     if (error) {
+//         console.log(error);
+//         return;
+
+
+//     }  
+//     console.log('Directory read successfully');
+//     console.log(files);
+
+
+// })
+
+
+
+// url
 
 // const url = new URL("https://google.com/huddleNepal")
 
@@ -99,10 +181,67 @@
 //   }
 // });
 
-// console.log(newUrl); 
+// console.log(newUrl);
 
 
-// creating http server 
+
+
+
+// Event-Driven Programming
+
+// const event = require('events')
+// const myEmitter = new event()
+
+// // Listener Function 1: display
+// const display = ()=>{
+//     console.log('Hi, Welcome to server');
+
+// }
+
+// // Listener Function 2: output
+// const output = ()=>{
+//     console.log('Hi, Welcome to output');
+
+// }
+
+// // Subscribing to `userJoined` event
+// myEmitter.on('userJoined',display)
+// // there can be multiple on for single emit()
+// myEmitter.on('userJoined',output)
+
+
+// // Emiting the `userJoined` Event
+// myEmitter.emit('userJoined')
+
+
+
+
+// Project: Login Alert System using EventEmitter
+
+
+// const EventEmitter = require('events')
+
+// const myEmitter = new EventEmitter()
+
+// const userLoggedin = (name, age) => {
+
+//     console.log(`your age: ${age}`);
+
+//     console.log(`welcome , ${name}! You have successfully loggrd in.`);
+
+
+// }
+// myEmitter.on('Loggedin', userLoggedin)
+
+// myEmitter.emit('Loggedin', 'Smriti', 21)
+
+
+
+
+// The HTTP Module:allow communication betwee client and server.
+
+
+// creating http server
 
 
 // const http = require('http')
@@ -151,7 +290,7 @@
 // You can also write all data in res.end()
 //  directly without res.write():
 
-// Basic Routing Server 
+// Basic Routing Server
 
 // const http = require('http')
 // const server = http.createServer((req, res) => {
