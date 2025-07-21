9c3e88f496ea58b757e127585005cba75b16d9ca
Smirti Neupane <smirtineupane197@gmail.com>
2025-07-21 09:47:44 +0545

    Added Simple-blog

--- simple-blog/app.js
+++ simple-blog/app.js
@@ -3,23 +3,34 @@ const path = require('path')
 
 const app = express()
 
-app.set('view engine','ejs')
+let posts = [{ title: "My Day", content: "It was great." },
+{ title: "Study", content: "Learned EJS." }];
+app.set('view engine', 'ejs')
 
-app.set('views',path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/',(req,res)=>{
-    res.render("home",{})
+app.get('/', (req, res) => {
+    res.render("home", { posts })   //render home.ejs and send 'posts' to home.ejs
 })
 
-app.get('/new',(req,res)=>{
-    
+// New post form 
+app.get('/new', (req, res) => {
+    res.render("new")
 })
 
-app.listen(3000,()=>{
+//Handles post submission
+app.post('/new', (req, res) => {
+    const { title, content } = req.body;
+    posts.push({ title, content })
+    res.redirect('/')
+
+})
+
+app.listen(3000, () => {
     console.log('Server is listening at localhost:3000');
-    
+
 })
