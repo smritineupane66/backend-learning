1d58f7214efac7011962fec2d5305008f0169f3a
Smirti Neupane <smirtineupane197@gmail.com>
2025-07-23 23:19:04 +0545

    Initial Setup of MongoDb

---
+++ MongoDb/server.js
@@ -0,0 +1,10 @@
+//  What is MongoDB?
+// MongoDB is a NoSQL database that stores data
+//  in JSON-like documents called BSON (Binary JSON).
+// Instead of rows and tables like SQL, 
+// it uses collections and documents.
+
+
+//  What is Mongoose ?
+//     Mongoose is an ODM(Object Data Modeling) library for MongoDB in Node.js.
+// It helps you define schemas and models and makes interacting with MongoDB easier.
