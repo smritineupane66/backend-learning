d771ef570dd32e34c504a4c28c77eabd3853e2a9
Smirti Neupane <smirtineupane197@gmail.com>
2025-07-26 16:07:12 +0545

    Refactor MongoDB setup and update folder name

--- MongoDb/server.js
+++ MongoDb/server.js
@@ -1,41 +1,31 @@
-//  What is MongoDB?
-// MongoDB is a NoSQL database that stores data
-//  in JSON-like documents called BSON (Binary JSON).
-// Instead of rows and tables like SQL, 
-// it uses collections and documents.
+//  MongoDB
 
 
-//  What is Mongoose ?
-//     Mongoose is an ODM(Object Data Modeling) library for MongoDB in Node.js.
-// It helps you define schemas and models and makes interacting with MongoDB easier.
-
-
-const mongoose = require('mongoose')
 
 //connect to MongoDB
 
-mongoose.connect("mongodb://localhost:27017/testDB")
- .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(" Connection error", err));
+const express = require('express')
+const mongoose = require('mongoose')
 
-//define schemas
+const app = express();
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+app.use(express.json())
+
+
+app.listen(8000, async () => {
+    await mongoose.connect('mongodb+srv://smirtineupane197:SmritiAtlas%40123@myfirstcluster.wzksvlb.mongodb.net/testDB?retryWrites=true&w=majority&appName=MyFirstCluster')
+
+    console.log('Server has Started');
+
+    try {
+        console.log('Connected to DB');
+        
+
+    } catch (error) {
+        console.log(error);
+
+
+    }
 })
 
-const User = mongoose.model("User", userSchema)
 
-
-//Insert Data
-
-const user = new User({ name: 'smriti', age: 21 })
-user.save()
-    .then(() => console.log("User Inserted "))
-    .catch((err) => console.log("Error", err)
-    )
-
-User.find()
-    .then(users => console.log(" All Users:", users))
-    .catch(err => console.log(" Fetch error", err));
